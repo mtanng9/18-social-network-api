7f4030c73af917f624628b2dfbbbb5c3c0cab4f7
My Tang <my@192.168.100.8>
2023-04-29 23:12:20 +0700

    created basic schema and model for user, thought, and reaction

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function(v) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+        },
+        message: props => `${props.value} is not a valid email!`
+      },
+    },
+    thoughts: [      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+userSchema.virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
